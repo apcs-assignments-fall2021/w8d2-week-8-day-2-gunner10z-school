cfad0181de5774ca972dd0c20eaf7b47c75d6151
gunner10z-school <24jzweiback@athenian.org>
2021-10-20 14:12:36 -0700

    first

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -27,32 +27,50 @@ public class Rational {
     // also call it in this method to return the simplified result)
     public static Rational add(Rational r, Rational s) {
         // REPLACE WITH YOUR CODE HERE
-        return null;
+        int rTempNum=r.numerator*s.denominator;
+        int rTempDenom=r.denominator*s.denominator;
+        int sTempNum=s.numerator*r.denominator;
+        int sTempDenom=s.denominator*r.denominator;
+        Rational t = new Rational(rTempNum+sTempNum, rTempDenom);
+        Rational u= simplify(t);
+        return u;
     }
 
     // This method takes two Rationals, subtracts thems up, 
     // and returns a Rational equal to the difference
     public static Rational subtract(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int rTempNum=r.numerator*s.denominator;
+        int rTempDenom=r.denominator*s.denominator;
+        int sTempNum=s.numerator*r.denominator;
+        int sTempDenom=s.denominator*r.denominator;
+        Rational t = new Rational(rTempNum-sTempNum, rTempDenom);
+        Rational u= simplify(t);
+        return u;
     }
     
     public static Rational multiply(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        Rational t=new Rational(r.numerator*s.numerator,r.denominator*s.denominator);
+        Rational u= simplify(t);
+        return u;
     }
     
     public static Rational divide(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        Rational t=new Rational(r.numerator*s.denominator,r.denominator*s.numerator);
+        Rational u= simplify(t);
+        return u;
     }
 
     // Finds the greatest common factor between a and b
     // To find the greatest common factor, find the largest number x
     // such that a and b are both multiples of x
     public static int greatestCommonFactor(int a, int b){
-        // REPLACE WITH YOUR CODE HERE
-        return 1;
+        int GCF=0;
+        for (int i=1;i<=Math.max(a,b);i++){
+            if (a%i==0&&b%i==0){
+                GCF=i;
+            }
+        }
+        return GCF;
     }
 
     // This method is given a rational, and returns a simplified version
@@ -61,8 +79,9 @@ public class Rational {
     // e.g. simplify(2/4) => 1/2
     //      simplify(1/2) => 1/2
     public static Rational simplify(Rational r) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int GCF=greatestCommonFactor(r.numerator,r.denominator);
+        Rational t=new Rational(r.numerator/GCF,r.denominator/GCF);
+        return t;
     }
 
     // This following method is NOT static, we'll talk about it next class!
